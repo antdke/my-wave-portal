595fd5d27ea44d5c2e8513667b53a26421eb886b
Anthony Diké <anthonydike@nyu.edu>
2021-10-03 14:40:59 -0400

    chore: running the contract in run.js

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,9 +5,26 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 public totalWaves;
+
+    mapping(address => uint256) public addressToWaves;
+
     constructor() {
         console.log(
             "This is my smart contract. There are many like this. But, this one is mine."
         );
     }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved", msg.sender);
+    }
+
+    // repetetive, will delete
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getAddressToWaves() public view returns (uint256) {}
 }
