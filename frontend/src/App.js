f6ceb387f5f0b3626723dbc337971f9d82d4195f
Anthony Diké <anthonydike@nyu.edu>
2021-10-04 20:39:20 -0400

    feat: building frontedn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,64 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 //import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  /*
+  * State variable to store our user's public wallet
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  const wave = () => {
+  const checkIfWalletIsConnected = async () => {
+    try {
+      /*
+      * First, make sure we have access to window.ethereum
+      */
+      const { ethereum } = window;
 
+      if (!ethereum) {
+        console.log("Make sure you have metamask!")
+        return
+      } else {
+        console.log("We have the ethereum object:", ethereum)
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  /*
+    * Run function when page loads
+    */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
   return (
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
-          👋 Hey there!!!!!!!!!!
+          👋 Hey there!
         </div>
 
         <div className="bio">
           I'm Anthony :)
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
